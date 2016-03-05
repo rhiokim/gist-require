e14366aa926c64151f823257e2bc0edf05696b9c
rhiokim <rhio.kim@gmail.com>
2016-03-06 01:05:38 +0900

    update test code

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
 import test from 'ava';
 import gistr from './index.js';
 
-test(t => {
-    t.throws(gistr)
-})
+// test(t => {
+//     t.throws(gistr)
+// })
 
-test.cb(t => {
+test('baic request', t => {
     let url = 'https://gist.github.com/rhiokim/d4b2a3fa327df4266f1c'
     gistr(url).then(data => {
         if (data) {
             t.ok(data);
             t.notThrows(data);
+            t.is(data.length, 3);
+            t.is(data[0].filename, 'bar.md');
         }
     }).catch(err => {
         // Github API rate limiting for travis servers...
